1f1a485ced6ac4bb60a83050506a421b08fd6c8e
EmilLubomirov <emil_lubomirov_1999@abv.bg>
2020-08-01 16:18:50 +0300

    Update

---
+++ js/scrollAnimation.js
@@ -0,0 +1,26 @@
+function scrollAnimation() {
+
+    const articles = document.querySelectorAll('.bikes-collection .bike');
+    let prevViewportHeight = 0;
+    const effectVal = 70;
+
+    const viewportHeight = window.innerHeight;
+
+    if (prevViewportHeight > viewportHeight){
+        return;
+    }
+
+    for (const article of Array.from(articles)){
+        const rect = article.getBoundingClientRect();
+        const articlePositionTop = rect.top + effectVal;
+
+        if (articlePositionTop < viewportHeight){
+            article.style.opacity = '1';
+        }
+    }
+
+    prevViewportHeight = viewportHeight;
+}
+
+scrollAnimation();
+window.addEventListener('scroll', scrollAnimation);
